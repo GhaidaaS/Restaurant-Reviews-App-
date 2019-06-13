c747c24a7b2993a7ddb82e708bea49718a3d1d6a
Ghaidaa Saeed <ghaidaa.s17@gmail.com>
2019-06-13 17:26:21 +0300

    Update restaurant_info.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -34,8 +34,6 @@ initMap = () => {
     }
   });
 }  
- 
-
 
 /**
  * Get current restaurant from page URL.
@@ -75,7 +73,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-
+  image.alt="Image of "+`${restaurant.name} Restaurant`;
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
   cuisine.tabIndex=0;
@@ -116,7 +114,7 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
-  const title = document.createElement('h2');
+  const title = document.createElement('h3');
   title.innerHTML = 'Reviews';
   container.appendChild(title);
 
